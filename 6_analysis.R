fdd617ecb9887220f41b3c5f67ecd9fa8c9e1ae3
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-24 19:40:10 +0000

    Initial attempt at modelling results

--- 6_analysis.R
+++ 6_analysis.R
@@ -7,6 +7,10 @@ cat (paste0 ('\nStage 6 started at [', Sys.time (), ']'))
 
 # LIBS
 library (dplyr)
+library(lme4)
+library (ggplot2)
+library (multcomp)
+source (file.path ('tools', 'plotting_tools.R'))
 
 # DIRS
 input.dir <- '5_metrics'
@@ -19,14 +23,83 @@ if (!file.exists (output.dir)) {
 cat ('\nReading in data ....')
 # read in RDS
 predicts.data <- readRDS (file.path(input.dir, 'predictsdata_wpd.rds'))
-# convert to dplyr format
-predicts.data <- tbl_df (predicts.data)
+# convert to by site
+site_ids <- paste0 (predicts.data$SSID, predicts.data$Site_number)
+predicts.data <- predicts.data[!duplicated (site_ids), ]
 
 # PROCESS
+
 # how many species made it into the trees?
-cat ('Mean [', 1 - mean (predicts.data$PD_pdropped), '] species in tree distritbution', sep = '')
-# how does PD respond to use intensity?
-plot (predicts.data$Est_mean_PD~predicts.data$Use_intensity)  # VERY crude plot
+cat ('Mean [', (1 - mean (predicts.data$PD_pdropped)) * 100,
+     '%] species in tree distritbution', sep = '')
+sum (predicts.data$Use_intensity == 'Cannot decide')/ nrow (predicts.data)
+sum (predicts.data$Use_intensity == 'Minimal use')/ nrow (predicts.data)
+sum (predicts.data$Use_intensity == 'Light use')/ nrow (predicts.data)
+sum (predicts.data$Use_intensity == 'Intense use')/ nrow (predicts.data)
+
+# CRUDE MODELLING OF PD
+plot (predicts.data$Est_mean_PD~predicts.data$Use_intensity)
+hist (predicts.data$Est_mean_PD)
+m1.pd <- lmer(Est_mean_PD ~ 1 + (1|SSID), data = predicts.data, REML = FALSE)
+m2.pd <- lmer(Est_mean_PD ~ Use_intensity + (1|SSID),
+           data = predicts.data, REML = FALSE)
+m3.pd <- lmer(Est_mean_PD ~ Predominant_habitat + (1|SSID),
+              data = predicts.data, REML = FALSE)
+m4.pd <- lmer(Est_mean_PD ~ Use_intensity + (1|Predominant_habitat) + (1|SSID),
+              data = predicts.data, REML = FALSE)
+summary(m1.pd)
+summary(m2.pd)
+summary(m3.pd)
+summary(m4.pd)
+qqnorm(resid(m2.pd))
+qqline(resid(m2.pd))
+plot(resid(m2.pd))
+plot(resid(m2.pd) ~ fitted(m2.pd))
+abline(h=0)
+plot(predicts.data$Est_mean_PD ~ fitted(m2.pd))
+abline(a=0,b=1)
+dotplot(ranef(m2,pd))
+anova(m1.pd, m2.pd, m3.pd, m4.pd)  # m4 is best model?
+# plot -- http://stackoverflow.com/questions/9447329/how-to-plot-the-results-of-a-mixed-model
+confints <- as.data.frame (confint (glht (m4.pd))$confint)
+# make relative to [1, ]
+confints[2, ] <- confints[1, ] + confints[2, ]
+confints[3, ] <- confints[1, ] + confints[3, ]
+confints[4, ] <- confints[1, ] + confints[4, ]
+confints$use <- c ('Minimal use', 'Light use', 'Intense use', 'Cannot decide')
+ggplot(confints, aes(x = use, y = Estimate, ymin = lwr, ymax = upr)) +
+  geom_errorbar() + geom_point() + xlab ('Use Intensity') + ylab ('PD')
+
+# CRUDE MODELLING OF PSV
+plot (predicts.data$Est_mean_PSV~predicts.data$Use_intensity)
+hist (predicts.data$Est_mean_PSV)
+m1.psv <- lmer(Est_mean_PSV ~ 1 + (1|SSID), data = predicts.data, REML = FALSE)
+m2.psv <- lmer(Est_mean_PSV~ Use_intensity + (1|SSID),
+           data = predicts.data, REML = FALSE)
+m3.psv <- lmer(Est_mean_PSV~ Predominant_habitat + (1|SSID),
+               data = predicts.data, REML = FALSE)
+m4.psv <- lmer(Est_mean_PSV~ Use_intensity + (1|SSID) + (1|Predominant_habitat),
+               data = predicts.data, REML = FALSE)
+summary(m1.psv)
+summary(m2.psv)
+anova(m1.psv,m2.psv, m3.psv, m4.psv)  # m4 is best again?
+# plot
+confints <- as.data.frame (confint (glht (m4.psv))$confint)
+# make relative to [1, ]
+confints[2, ] <- confints[1, ] + confints[2, ]
+confints[3, ] <- confints[1, ] + confints[3, ]
+confints[4, ] <- confints[1, ] + confints[4, ]
+confints$use <- c ('Minimal use', 'Light use', 'Intense use', 'Cannot decide')
+ggplot(confints, aes(x = use, y = Estimate, ymin = lwr, ymax = upr)) +
+  geom_errorbar() + geom_point() + xlab ('Use Intensity') + ylab ('PSV')
+
+# CRUDE MODELLING OF PSE
+m1.pse <- lmer(as.factor(Est_mean_PSE) ~ 1 + (1|SSID), data = predicts.data, REML = FALSE)
+m2.pse <- lmer(as.factor(Est_mean_PSE)~ as.factor(Use_intensity) + (1|SSID),
+           data = predicts.data, REML = FALSE)
+summary(m1.pse)
+summary(m2.pse)
+anova(m1.pse,m2.pse)
 
 # OUTPUT
 
---
+++ tools/plotting_tools.R
@@ -0,0 +1,57 @@
+# 24/02/2014
+# Team PREDICTS-PD
+# Plotting tools
+
+plot.fixed.effects <- function(model, main, sub = I(formula(model))){
+  stopifnot(class(model)=="mer")
+  
+  split.effect.names<-function(effect.names, factors){
+    #Splits effect names into LHS (factor) and RHS (level) for later labelling
+    to.return <- data.frame(full.name=effect.names, factor=rep(NA, length(effect.names)), 
+                            level=rep(NA, length(effect.names)))
+    for (i in 1:length(factors)){
+      matches <- grep(factors[i], effect.names)  
+      to.return$factor[matches] <- factors[i]
+      to.return$level[matches] <- substring(effect.names[matches], nchar(factors[i]) + 1)
+    }
+    return(to.return)
+  }  
+  
+  factor_names <- attr(attr(model@frame,"terms"),"term.labels")
+  effect.names <- split.effect.names(names(fixef(model)),factor_names)
+  if(is.null(model@call$family)){
+    y <- fixef(model)[-1]
+    ysmall <- fixef(model) - 1.96 * summary(model)@coefs[,2]
+    ybig <- fixef(model) + 1.96 * summary(model)@coefs[,2]
+    ylab.1 <- ""
+    baseline = 0
+  }else{  
+    if (model@call$family == "poisson"){
+      y <- exp(fixef(model))[-1]
+      ysmall <- exp(fixef(model) - 1.96 * summary(model)@coefs[,2])
+      ybig <- exp(fixef(model) + 1.96 * summary(model)@coefs[,2])
+      ylab.1 <- "back-transformed "
+      baseline <- 1
+    }else{
+      print("Not implemented yet")
+      stop("Plot not implemented yet for this family")
+    }
+  }
+  max.y <- ceiling(max(y))
+  min.y <- floor(min(y))
+  baseline.level <- levels(model@frame[,2])[1]
+  ylab <- paste(ylab.1, "fits (", baseline.level," = ",baseline,")", sep="")
+  sub.parts <- as.character(formula(model))
+  sub <- paste(sub.parts[2], sub.parts[1], sub.parts[3])
+  
+  cex <- 1/sqrt(par("mfrow")[2])
+  #cex<-1.5
+  plot(y, ylim=c(min.y, max.y), xlim=c(0.75, length(y)+0.25),ylab=ylab,xlab="Fixed effect", pch=19, main=main, sub=sub, cex.lab=cex,
+       cex.axis = cex, cex.sub=cex)
+  abline(h=baseline,col="red")
+  for (i in 2:length(fixef(model))){
+    lines(c(i-1,i-1),c(ysmall[i],ybig[i]))
+    text(i-1, min.y, effect.names$factor[i], srt=90, adj=0,col="gray", cex=cex)
+    text(i-1, max.y, effect.names$level[i], srt=90, adj=1,col="gray", cex=cex)
+  }
+}
