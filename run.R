e562085722469649092a9b69784cc9ecd66b5f1d
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-03-24 18:31:07 +0000

    Minor edit

--- run.R
+++ run.R
@@ -3,8 +3,8 @@
 ## Run all stages (reproducibility button)
 
 ## Process
-source ('1_pGltsetup.R', eval = TRUE)
+#source ('1_pGltsetup.R', eval = TRUE)
 #source ('2_pGltrun.R', eval = TRUE)
 source ('3_parse.R', eval = TRUE)
-source ('4_compare.R', eval = TRUE)
+#source ('4_compare.R', eval = TRUE)
 source ('5_analysis.R', eval = TRUE)
