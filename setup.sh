0743a5bfad8396c07f572761089ac1abb795ad1d
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-04-24 16:24:37 +0100

    Created setup pipeline
    — rate smooth published trees
    — resolve names to make mapping faster

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 24/04/2014
+# Team PREDICTS-PD
+# Run all setup R scripts for R pipeline
+
+echo 'Running pipeline setup'
+Rscript stages/setup_preresolve.R >& setup_preresolve_log.txt
+Rscript stages/setup_parse.R >& setup_parse_log.txt
+echo 'Complete'
---
+++ stages/setup_parse.R
@@ -0,0 +1,34 @@
+# 24/04/2015
+# Team PREDICTS-PD
+# Read in raw trees, rate-smooth and output
+
+# LIBS
+library (ape)
+
+# DIRS
+input.dir <- file.path ('0_data', 'raw_trees')
+output.dir <- file.path ('0_data', 'parsed_trees')
+
+# PROCESS
+treefiles <- list.files (input.dir, pattern = '.tre')
+trees <- list ()
+for (i in 1:length (treefiles)) {
+  tree <- read.tree (file.path (input.dir, treefiles[i]))
+  if (class (tree) == 'multiPhylo') {
+    random <- sample (1:length (tree), 1)
+    tree <- tree[[random]]
+  }
+  # remove node labels
+  tree$node.label <- NULL
+  # add to list
+  trees[treefiles[i]] <- list (tree)
+}
+
+# RATE-SMOOTH
+# TODO
+
+# WRITE OUT
+for (i in 1:length (trees)) {
+  outfile <- file.path (output.dir, names (trees)[i])
+  write.tree (phy=trees[[1]], file=outfile)
+}
