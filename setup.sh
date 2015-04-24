889c5032f9418f05c2e0a22f5f072a296e1d8c60
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-04-24 17:12:57 +0100

    Minor change

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # 24/04/2014
 # Team PREDICTS-PD
 # Run all setup R scripts for R pipeline
+# Usage: sh setup.sh
 
 echo 'Running pipeline setup'
 Rscript stages/setup_preresolve.R >& setup_preresolve_log.txt
