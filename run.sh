1f15925326fe68c02658d08f71044eee654a51b1
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-04-24 16:23:50 +0100

    Replaced with a shell script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 01/11/2014
+# Team PREDICTS-PD
+# Run and log all stages -- except stages 2 and 7
+
+echo 'Running pipeline'
+Rscript stages/pGltsetup.R >& 1_pGltsetup_log.txt
+Rscript stages/map.R >& 3_map_log.txt
+Rscript stages/parse.R >& 4_parse_log.txt
+Rscript stages/compare.R >& 5_compare_log.txt
+Rscript stages/metrics.R >& 6_metrics_log.txt
+echo 'Complete'
