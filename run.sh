9c9f98836a2a01ff889ad0857531db2bdff92e42
Dom Bennett <dominic.john.bennett@gmail.com>
2015-06-22 14:50:55 +0100

    Remove hashes

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 echo 'Running pipeline'
 Rscript stages/pGltsetup.R > 1_pGltsetup_log.txt 2>&1
 Rscript stages/map.R > 3_map_log.txt 2>&1
-#Rscript stages/parse.R 2>& 4_parse_log.txt
-#Rscript stages/compare.R 2>& 5_compare_log.txt
-#Rscript stages/metrics.R 2>& 6_metrics_log.txt
-#Rscript stages/commplots.R 2>& 7_commplots_log.txt
+Rscript stages/parse.R 2>& 4_parse_log.txt
+Rscript stages/compare.R 2>& 5_compare_log.txt
+Rscript stages/metrics.R 2>& 6_metrics_log.txt
+Rscript stages/commplots.R 2>& 7_commplots_log.txt
 echo 'Complete'
