6a048e918a812ac961d76e87f72d081b0524c980
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-09-01 14:16:52 +0100

    Created run.R
    — now loop certain stages

---
+++ run.R
@@ -0,0 +1,22 @@
+# 01/11/2014
+# Team PREDICTS-PD
+# Run pipeline (e.g. RScript run.R > run_log.txt 2>&1)
+
+# Parameters
+parameters <- list ('normalise'=c ('age', 'total'),
+                    'use.unconstrained'=c (TRUE, FALSE))
+
+# Pipeline
+cat ('\nRunning pipeline ....')
+source ('stages/pGltsetup.R', print.eval=TRUE, local=TRUE)
+# loop through compare stages with constrained and unconstrained
+for (use.unconstrained in parameters[['use.unconstrained']]) {
+  source ('stages/parse.R', print.eval=TRUE, local=TRUE)
+  source ('stages/compare.R', print.eval=TRUE, local=TRUE)
+}
+# loop metrics for the two ways of normalising branch lengths
+for (normalise in parameters[['normalise']]) {
+  source ('stages/metrics.R', print.eval=TRUE, local=TRUE)
+}
+source ('stages/commplots.R', print.eval=TRUE, local=TRUE)
+cat ('\nComplete....')
