8c24de18da4ec415dd5db330c5e7a1da01ee4a16
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-09-07 14:35:27 +0100

    Remove parameters list

--- run.R
+++ run.R
@@ -4,19 +4,19 @@
 
 # Parameters
 ncpus <- 8
-parameters <- list ('normalise'=c ('age', 'total'),
-                    'use.unconstrained'=c (TRUE, FALSE))
+use.unconstrained.vec <- c (TRUE, FALSE)
+normalise.vec <- c ('age', 'total')
 
 # Pipeline
 cat ('\nRunning pipeline ....')
 source ('stages/pGltsetup.R', print.eval=TRUE, local=TRUE)
 # loop through compare stages with constrained and unconstrained
-for (use.unconstrained in parameters[['use.unconstrained']]) {
+for (use.unconstrained in use.unconstrained.vec) {
   source ('stages/parse.R', print.eval=TRUE, local=TRUE)
   source ('stages/compare.R', print.eval=TRUE, local=TRUE)
 }
 # loop metrics for the two ways of normalising branch lengths
-for (normalise in parameters[['normalise']]) {
+for (normalise in normalise.vec) {
   source ('stages/metrics.R', print.eval=TRUE, local=TRUE)
 }
 source ('stages/commplots.R', print.eval=TRUE, local=TRUE)
